36793d9721a4c269329cdeb05f8859fa7dc5f466
dhKang14 <139040057+dhKang14@users.noreply.github.com>
2024-01-16 00:58:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ import base64
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from datetime import datetime 
 from sqlalchemy import func
+from matplotlib import font_manager, rc
+from flask_socketio import SocketIO, emit
 
 
 app = Flask(__name__)
@@ -33,6 +35,7 @@ openai.api_key = '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'
 # MySQL 데이터베이스 연결 설정
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:madmad@localhost/music_database'
 db = SQLAlchemy(app)
+socketio = SocketIO(app)
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -71,6 +74,21 @@ class UserSong(db.Model):
 
     # UserSong 모델과 User 모델 간의 관계 정의
     user = db.relationship('User', back_populates='user_songs')
+
+# 채팅 메시지 모델
+class ChatMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.String(50))
+    user_id = db.Column(db.String(50))
+    message = db.Column(db.String(500))
+    timestamp = db.Column(db.DateTime)
+
+# 채팅방 모델
+class ChatRoom(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(50))
+
+
 
 
 @app.route('/')
@@ -149,9 +167,11 @@ def logout():
 
 
 #user 프로필 반환
-@app.route('/users/<user_id>', methods=['GET'])
-def get_profile(user_id):
+@app.route('/users', methods=['GET'])
+def get_profile():
     try:
+        data = request.json
+        user_id = data.get('user_id')
         # user_id에 해당하는 유저 프로필 데이터 조회
         user = User.query.get(user_id)
 
@@ -327,22 +347,39 @@ def generate_lyrics_and_chord():
         return jsonify({'error': '장르, 좋아하는 노래 제목, 가수를 모두 입력하세요.'}), 400
 
     try:
+        # 제목 생성을 위한 message
+        title_message = [
+            {"role": "system", "content": "You are a helpful assistant that generates lyrics and chords."},
+            {"role": "user", "content": f"장르: {genre}\n\
+             좋아하는 노래: {favorite_song} by {favorite_artist}\n\
+            기반하여 새로운 노래의 제목을 제안해주세요. 다른 그 어떤 말도 없이 제목만 써주세요. 앞뒤 설명도 하지 마세요.\n\n"}
+        ]
+        
         # 가사 생성을 위한 message
         lyrics_message = [
             {"role": "system", "content": "You are a helpful assistant that generates lyrics and chords."},
-            {"role": "user", "content": f"장르: {genre}\n좋아하는 노래: {favorite_song} by {favorite_artist}\n기반하여 새로운 노래의 제목, 가사를 제안해주세요.\n\n"}
+            {"role": "user", "content": f"장르: {genre}\n\
+             좋아하는 노래: {favorite_song} by {favorite_artist}\n\
+            기반하여 새로운 노래의 가사를 제안해주세요. 다른 그 어떤 말도 없이, 가사만 주세요. 앞뒤 설명도 하지 마세요.\n\n"}
         ]
+        # 코드진행 생성을 위한 message
+        chord_message = [
+            {"role": "system", "content": "You are a helpful assistant that generates lyrics and chords."},
+            {"role": "user", "content": f"장르: {genre}\n\
+             좋아하는 노래: {favorite_song} by {favorite_artist}\n\
+            기반하여 새로운 노래의 코드진행을 제안해주세요.가사 없이 코드만 주세요. 다른 그 어떤 말도 없이, 코드만 주세요. 앞뒤 설명도 하지 마세요.\n\n"}
+        ]
+        title_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=title_message,
+            max_tokens=150
+        )
         lyrics_response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=lyrics_message,
             max_tokens=150
         )
-
-        # 코드진행 생성을 위한 message
-        chord_message = [
-            {"role": "system", "content": "You are a helpful assistant that generates lyrics and chords."},
-            {"role": "user", "content": f"장르: {genre}\n좋아하는 노래: {favorite_song} by {favorite_artist}\n기반하여 새로운 노래의 코드진행을 제안해주세요.\n\n"}
-        ]
+        
         chord_response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=chord_message,
@@ -350,6 +387,7 @@ def generate_lyrics_and_chord():
         )
 
         return jsonify({
+            'generated_title' : title_response.choices[0].message["content"].strip(),
             'generated_lyrics': lyrics_response.choices[0].message["content"].strip(),
             'generated_chord': chord_response.choices[0].message["content"].strip()
         }), 200
@@ -401,7 +439,7 @@ def generate_image():
 
 def letmeknow_genre(song_title, singer_name):
     try:
-        # GPT-4를 사용하여 노래의 장르 예측
+        # gpt-3.5를 사용하여 노래의 장르 예측
         message = (
             "'발라드', 'k-pop', '댄스', '랩/힙합', 'R&B/Soul', '인디음악', '록/메탈', '트로트', '그 외' 중에서 한 가지를 꼽자면 "
             f"'{singer_name}'의 '{song_title}'은 어떤 장르인지 반드시 한마디(장르이름)로 답변해줘. 절대로 장르 외에 다른말을 붙이면 안돼. ex)발라드"
@@ -496,21 +534,29 @@ def play_song():
 #월별로 볼수있게->month 선택할 수 있게 
     
 #월간로그-특정 user_id 사용자가 총 음악들은 횟수 반환
-def get_total_play_count(user_id):
+@app.route('/total-play-count', methods=['GET'])
+def get_total_play_count():
     try:
+        data = request.json
+        user_id = data.get('user_id')
+        current_year = datetime.utcnow().year
+        current_month = datetime.utcnow().month
         # 특정 user_id에 해당하는 사용자의 모든 노래의 play_count 합계를 쿼리로 계산
-        total_play_count = db.session.query(db.func.sum(UserSong.play_count)).filter_by(user_id=user_id).scalar()
-        return total_play_count
+        total_play_count = db.session.query(db.func.sum(UserSong.play_count)).filter_by(user_id=user_id, year=current_year, month=current_month).scalar()
+        return jsonify({'total_play_count': total_play_count}), 200
     except Exception as e:
-        print(e)
-        return None
+        print(str(e))
+        return jsonify({'error': '총 음악 횟수를 가져오는 동안 오류가 발생했습니다.'}), 500
+
 
 
 
 #월간로그-특정 user_id 사용자가 많이 들은 가수
-@app.route('/most-listened-singer/<user_id>', methods=['GET'])
-def most_listened_singer(user_id):
+@app.route('/most-listened-singer', methods=['GET'])
+def most_listened_singer():
     try:
+        data = request.json
+        user_id = data.get('user_id')
         # 현재 연도와 월을 가져옵니다.
         current_year = datetime.utcnow().year
         current_month = datetime.utcnow().month
@@ -538,9 +584,11 @@ def most_listened_singer(user_id):
 
 
 #월간로그-특정 user_id 사용자가 많이 들은 장르
-@app.route('/most-listened-genre/<user_id>', methods=['GET'])
-def most_listened_genre(user_id):
+@app.route('/most-listened-genre', methods=['GET'])
+def most_listened_genre():
     try:
+        data = request.json
+        user_id = data.get('user_id')
         # 현재 연도와 월을 가져옵니다.
         current_year = datetime.utcnow().year
         current_month = datetime.utcnow().month
@@ -569,9 +617,11 @@ def most_listened_genre(user_id):
 
 #월간로그-특정 user_id 사용자의 최다감상곡
 # 이번 달 가장 많이 들은 최다 감상 곡 조회 API 엔드포인트
-@app.route('/most-listened-song/<user_id>', methods=['GET'])
-def most_listened_song(user_id):
+@app.route('/most-listened-song', methods=['GET'])
+def most_listened_song():
     try:
+        data = request.json
+        user_id = data.get('user_id')
         # 현재 연도와 월을 가져옵니다.
         current_year = datetime.utcnow().year
         current_month = datetime.utcnow().month
@@ -600,6 +650,12 @@ def most_listened_song(user_id):
 
 
 #월간 장르 통계
+# 한글 폰트 설치 경로를 지정합니다. 실제 경로는 시스템에 따라 다를 수 있습니다.
+font_path = '/root/NanumSquareRoundR.ttf'
+
+# 폰트를 로드합니다.
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
 
 # 이번 달에 해당하는 사용자의 노래 재생 기록 가져오기
 def month_genre(user_id):
@@ -648,9 +704,11 @@ def month_genre(user_id):
         return {'error': '장르 비율을 가져오는 동안 오류가 발생했습니다.'}
 
 
+    
+
+#정 안되면 프론트에서...
 #월간 파이차트 생성
-#월간 파이차트 생성
-@app.route('/month-genre-chart')
+@app.route('/month-genre-chart',methods=['GET'])
 def month_genre_chart():
     data = request.json
     user_id = data.get('user_id')
@@ -679,15 +737,131 @@ def month_genre_chart():
 
 
 
-    
+
 
 
 
 
 #주간 장르 통계
+#주간 파이차트 생성
+# 이번 달에 해당하는 사용자의 노래 재생 기록 가져오기
+def week_genre(user_id):
+    try:
+        # 현재 날짜 및 시간 정보 가져오기
+        current_date = datetime.utcnow()
+
+    # 현재 년도, 월, 주(Week) 정보 가져오기
+        current_year = current_date.year
+        current_month = current_date.month
+        current_week = current_date.isocalendar()[1]  # ISO 주(Week) 정보
+
+        user_songs = UserSong.query.filter_by(
+            user_id=user_id,
+            year=current_year,
+            month=current_month,
+            week=current_week
+        ).all()
+
+        if not user_songs:
+            return jsonify({'error': '이번 달에 들은 노래가 없습니다.'}), 404
+
+        genre_counts = {}
+        total_play_count = 0
+
+        for song in user_songs:
+            total_play_count += song.play_count
+            if song.genre:
+                if song.genre in genre_counts:
+                    genre_counts[song.genre] += song.play_count
+                else:
+                    genre_counts[song.genre] = song.play_count
+
+        # 각 장르별 비율을 계산하고 스케일링하여 합을 100%로 조절
+        total_scaled_ratio = 0
+        scaled_genre_ratio = {}
+        for genre, count in genre_counts.items():
+            ratio = (count / total_play_count) * 100
+            scaled_ratio = round(ratio, 2)  # 원하는 소수점 자릿수로 반올림
+            scaled_genre_ratio[genre] = scaled_ratio
+            total_scaled_ratio += scaled_ratio
+
+        # 합을 다시 계산하여 100%인지 확인하고 조절
+        if total_scaled_ratio != 100:
+            scaling_factor = 100 / total_scaled_ratio
+            for genre, ratio in scaled_genre_ratio.items():
+                scaled_genre_ratio[genre] = round(ratio * scaling_factor, 2)
+        print('genre_ratio')
+        return {'genre_ratio': scaled_genre_ratio}
+    except Exception as e:
+        print(str(e))
+        return {'error': '장르 비율을 가져오는 동안 오류가 발생했습니다.'}
+
+
+
+@app.route('/week-genre-chart', methods=['GET'])
+def week_genre_chart():
+    data = request.json
+    user_id = data.get('user_id')
+    genre_data = week_genre(user_id)
     
+    if 'error' in genre_data:
+        return genre_data['error'], 404
+
+    # 장르와 비율을 분리하여 리스트로 저장
+    labels = list(genre_data['genre_ratio'].keys())
+    sizes = list(genre_data['genre_ratio'].values())
+
+    # 파이 차트 생성
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', labeldistance=0.5)  # labeldistance 조정
+    ax.axis('equal')  # 원형을 유지
+
+    # 이미지 파일로 저장
+    image_name = f"user_{user_id}_chart.png"
+    image_path = os.path.join('static', 'images', image_name)
+    plt.savefig(image_path, format='png', bbox_inches='tight', pad_inches=0, transparent=True)  # 배경 투명 설정
+
+    # 이미지 URL 생성 및 반환
+    image_url = url_for('static', filename=f'images/{image_name}', _external=True)
+    return jsonify({'image_url': image_url})
+
+
 
 #채팅방
+
+# 라우트: 모든 채팅방 목록을 반환
+@app.route('/chat/rooms', methods=['GET'])
+def get_chat_rooms():
+    chat_rooms = ChatRoom.query.all()
+    rooms = [{'id': room.id, 'name': room.name} for room in chat_rooms]
+    return jsonify(rooms)
+
+
+
+# 라우트: 특정 채팅방의 메시지를 가져오기
+@app.route('/chat/messages/<room_id>', methods=['GET'])
+def get_chat_messages(room_id):
+    messages = ChatMessage.query.filter_by(room_id=room_id).order_by(ChatMessage.timestamp).all()
+    messages_data = [{'user_id': msg.user_id, 'message': msg.message, 'timestamp': msg.timestamp} for msg in messages]
+    return jsonify(messages_data)
+
+
+
+
+# WebSocket 이벤트: 클라이언트에서 새 메시지를 보낼 때
+@socketio.on('new_message')
+def handle_new_message(data):
+    room_id = data['room_id']
+    user_id = data['user_id']
+    message = data['message']
+    
+    # 메시지를 데이터베이스에 저장
+    new_message = ChatMessage(room_id=room_id, user_id=user_id, message=message)
+    db.session.add(new_message)
+    db.session.commit()
+    
+    # 모든 클라이언트에게 실시간으로 메시지 전송
+    emit('receive_message', {'user_id': user_id, 'message': message, 'timestamp': new_message.timestamp}, room=room_id)
 
 
 
